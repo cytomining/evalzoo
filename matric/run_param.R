9f90f4e0d0089cd39c79e8b17d67527189cf9909
Shantanu Singh <shsingh@broadinstitute.org>
2022-12-19 21:56:23 -0500

    revert arbitrary loc and rerun

--- matric/run_param.R
+++ matric/run_param.R
@@ -1,14 +1,14 @@
-run_param <- function(param_file, results_root_dir = ".") {
+run_param <- function(param_file) {
   setwd(".")
   options(knitr.duplicate.label = "allow")
   params_list <- yaml::read_yaml(param_file)
   params_identifier <-
     stringr::str_sub(digest::digest(params_list), 1, 8)
-  params_list$results_root_dir <- results_root_dir
+  dir.create("results", showWarnings = TRUE)
   rmarkdown::render(
     "0.knit-notebooks.Rmd",
     "github_document",
     params = params_list,
-    output_dir = file.path(results_root_dir, "results", params_identifier)
+    output_dir = file.path("results", params_identifier)
   )
 }
