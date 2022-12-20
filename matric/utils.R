24b6fd6e2889a43dc28405600d1ea4b807ac05a2
Shantanu Singh <shsingh@broadinstitute.org>
2022-12-20 09:38:07 -0500

    allow rendering anywhere

--- matric/utils.R
+++ matric/utils.R
@@ -12,29 +12,3 @@ print_git_hash <- function(repo_path) {
   print(glue::glue("Git remote of {repo_path} = {git_remote}"))
 }
 
-render_notebook <-
-  function(input, output_dir = "", ...) {
-    dir.create(output_dir,
-      showWarnings = FALSE,
-      recursive = TRUE
-    )
-
-    output_file <-
-      paste0(tools::file_path_sans_ext(input), ".md")
-
-    rmarkdown::render(
-      input = input,
-      output_file = output_file,
-      output_dir = output_dir,
-      output_format = "github_document",
-      quiet = TRUE,
-      ...
-    )
-
-    output_file_rel <-
-      file.path(output_dir, output_file)
-
-    read_lines(output_file_rel) %>%
-      str_remove_all(file.path(getwd(), output_dir, "")) %>%
-      write_lines(output_file_rel)
-  }
